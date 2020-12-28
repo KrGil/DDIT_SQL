4afbcb943a3c68b879c7a2c2ca4a26ccdeb1e67a
KrGil <kanjjang89@naver.com>
2020-12-28 18:42:55 +0900

    javaflex_sql add

---
+++ JAVAFLEX_sql.sql
@@ -0,0 +1,16 @@
+SELECT * from movie;
+SELECT ROWNUM,a.*
+FROM (SELECT movie_name, movie_viewcnt
+        FROM movie
+        ORDER BY movie_viewcnt DESC) a 
+where ROWNUM <=10;
+
+SELECT ROWNUM,a.*
+FROM (SELECT movie_name, movie_score
+        FROM movie
+        ORDER BY movie_score DESC) a 
+where ROWNUM <=10;
+
+
+
+
